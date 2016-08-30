4cde70142f37c48ef3a451765ba2ebc295d6cc61
utnso <christian.monasterios@gmail.com>
2016-08-30 01:11:26 -0300

    Servidor y Cliente v1

---
+++ Cliente1.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+int main(void) {
+struct sockaddr_in direccionServidor;
+direccionServidor.sin_family = AF_INET;
+direccionServidor.sin_addr.s_addr = inet_addr("10.0.2.15");
+direccionServidor.sin_port = htons(8089);
+int cliente = socket(AF_INET, SOCK_STREAM, 0);
+if (connect(cliente, (void*) &direccionServidor, sizeof(direccionServidor)) != 0) {
+perror("No se pudo conectar");
+return 1;
+}
+while (1) {
+char mensaje[1000];
+scanf("%s", mensaje);
+send(cliente, mensaje, strlen(mensaje), 0);
+}
+return 0;
+}
